ff40f4cd51bef96955134c1f401001e8f7012771
Farid Bellameche <faridautomatic@gmail.com>
2023-07-02 00:13:06 -0400

    ajustements

--- qcm_chain.py
+++ qcm_chain.py
@@ -8,17 +8,20 @@ from langchain.llms.base import BaseLLM
 
 from langchain.prompts import PromptTemplate
 
-template = """<|prompt|>You are a teacher preparing questions for a quiz. Given the following document, please generate 1 multiple-choice questions (MCQs) with 4 options and a corresponding answer letter based on the document.
-    Example question:
-    Question: question here
-    CHOICE_A: choice here
-    CHOICE_B: choice here
-    CHOICE_C: choice here
-    CHOICE_D: choice here
-    Answer: A or B or C or D
-    <Begin Document>
-    {doc}
-    <End Document></s><|answer|>"""
+instruction = """You are a teacher preparing questions for a quiz. Given the following document, please generate 1 multiple-choice questions (MCQs) with 4 options and a corresponding answer letter based on the document. Example question:\nQuestion: question here\nCHOICE_A: choice here\nCHOICE_B: choice here\nCHOICE_C: choice here\nCHOICE_D: choice here\Answer: A or B or C or D\<Begin Document>\n{doc}\n<End Document>"""
+template = f"Below is an instruction that describes a task. Write a response that appropriately completes the request.\n\n### Instruction:\n{instruction}\n\n### Response:"
+
+# template = """<|prompt|>You are a teacher preparing questions for a quiz. Given the following document, please generate 1 multiple-choice questions (MCQs) with 4 options and a corresponding answer letter based on the document.
+#     Example question:
+#     Question: question here
+#     CHOICE_A: choice here
+#     CHOICE_B: choice here
+#     CHOICE_C: choice here
+#     CHOICE_D: choice here
+#     Answer: A or B or C or D
+#     <Begin Document>
+#     {doc}
+#     <End Document></s><|answer|>"""
 
 
 PROMPT = PromptTemplate(
